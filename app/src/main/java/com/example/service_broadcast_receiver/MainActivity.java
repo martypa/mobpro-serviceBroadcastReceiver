7a90eed8350d0ea0dae8670f69f8ec3f7d2d21f1
Patrick Marty <marty.ruthi@outlook.com>
2019-03-21 16:23:44 +0100

    Init

---
+++ app/src/main/java/com/example/service_broadcast_receiver/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.service_broadcast_receiver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
